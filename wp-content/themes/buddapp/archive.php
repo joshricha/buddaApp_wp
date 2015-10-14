c36c65fd03dc5d87f26b9a1cab646e39725fa928
Josh Richardson <joshrichardson1234@hotmail.com>
2015-10-14 12:21:04 +1100

    Archive styled

--- wp-content/themes/buddapp/archive.php
+++ wp-content/themes/buddapp/archive.php
@@ -9,43 +9,56 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
+
+			<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+			
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			</section>
 
-				<?php
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+			<div class="container">
+				<div id="row">
+					<main id="content" class="col-sm-8">
+									
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; ?>
+							<?php
 
-			<?php the_posts_navigation(); ?>
+								/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/content', get_post_format() );
+							?>
 
-		<?php else : ?>
+						<?php endwhile; ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+						<?php the_posts_navigation(); ?>
+
+					<?php else : ?>
+
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</main><!-- content -->
+					
+					<!-- SIDE BAR -->
+					<aside class="col-sm-4">
+						<?php get_sidebar(); ?>
+					</aside>
 
-<?php get_sidebar(); ?>
+
+				</div>
+			</div>
+
+
 <?php get_footer(); ?>
