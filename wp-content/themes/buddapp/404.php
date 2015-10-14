bbde96754a203cc328521e3b6ca1d7cb1faf60c9
Josh Richardson <joshrichardson1234@hotmail.com>
2015-10-14 17:48:59 +1100

    404 improved

--- wp-content/themes/buddapp/404.php
+++ wp-content/themes/buddapp/404.php
@@ -9,50 +9,88 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+		<h1>Bummer, that page can't be found!</h1>
+	</section>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'buddapp' ); ?></h1>
-				</header><!-- .page-header -->
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-md-8">
+				<div class="error-404 not-found">
+					<div class="page-content">
+						<h2>Don't fret lets get you back on track</h2>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'buddapp' ); ?></p>
+						<!-- RESOURCES -->
+						<h3>Resources</h3>
+						<p>Perhaps you were looking for a specific resource?</p>
 
-					<?php get_search_form(); ?>
+						<div class="resource-row clearfix">
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+							<?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
 
-					<?php if ( buddapp_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'buddapp' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+							<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+					
+								<div class="resource">
+							    	
+									<img src="<?php echo get_field('resource_image')[url] ?>" alt="<?php echo get_field('resource_image')[ini_alter()] ?>">
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'buddapp' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+							    	<h3><a href="<?php echo get_field('resource_url') ?>"><?php the_title(); ?></a></h3>
+							    	
+									<?php the_excerpt(); ?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+									<?php if( !empty(get_field('button_text')) ) :  ?>
+							    	<a class="btn btn-success" href="<?php echo get_field('resource_url'); ?>"><?php echo get_field('button_text'); ?></a>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+							    <?php endif; ?>
+						    	</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+							<?php endwhile; ?>
+
+				    	</div><!-- resource row -->
+
+				    	<!-- CATEGORY -->
+				    	<h3>Categories</h3>
+				    	<p>Or maybe a popular category?</p>
+
+				    	<div class="widget widget_categories">
+				    		<h4 class="widget-title">
+				    			Most used categories
+				    		</h4>
+				    		<ul>
+				    			<?php wp_list_categories( array(
+
+				    				'orderby'	=> 'count',
+				    				'order'		=> 'DESC',
+				    				'show_count' => 1,
+				    				'title_li'	=> '',
+				    				'number'	=> 10
+
+
+				    			) ) ?>
+				    		</ul>
+				    	</div><!-- WIDGET -->
+
+				    	<!-- ARCHIVES -->
+						<h3>Archives</h3>
+						<p>You can always sort through our archives...</p>
+						<?php the_widget( 'WP_Widget_Archives', 'title=Our Archives' ); ?>
+
+
+
+
+					</div>
+				</div><!-- error -->
+			</main><!-- content -->
+
+			<!-- SIDEBAR -->
+
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+
+
+		</div><!-- primary -->
+	</div><!-- container -->
+	
 
 <?php get_footer(); ?>
--- wp-content/themes/buddapp/functions.php
+++ wp-content/themes/buddapp/functions.php
@@ -45,6 +45,7 @@ function buddapp_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'buddapp' ),
+		'footer' => esc_html__( 'Footer Menu', 'buddapp' )
 	) );
 
 	/*
